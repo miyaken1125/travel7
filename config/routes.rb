4270a01e743643a15aa3b745887b71daed5166cd
“miyaken1125” <kmkmkm0820@gmail.com>
2021-06-28 18:59:43 +0900

    travel7

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/new'
   get 'posts/show'
-  post 'create', to: 'posts#create', as: 'post_create'
+  
+  post 'posts/create', to: 'posts#create', as: 'post_create'
   
  
   get 'users/index'
   root 'users#new'
-  get 'users/show', to: 'users#show', as: 'show'
-  post 'create', to: 'users#create', as: 'user_create'
+# ↓to: 'users#show'のコントローラーを見に行く。
+  get 'users/:id/show', to: 'users#show', as: 'show'
+  post 'users/create', to: 'users#create', as: 'user_create'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
